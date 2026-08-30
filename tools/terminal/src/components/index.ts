0c5c0c82a7311b0438b8f205c2ac01b644933723
fincast-tool <psp4straw@googlemail.com>
2026-08-30 14:11:30 +0200

    Delete tools/terminal directory

--- tools/terminal/src/components/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './GrowthChart';
-export * from './ValuationCorridorChart';
-export * from './MultiplesChart';
-export * from '../types/valuation-chart';
-export * from '../utils/valuationEngine';
-export * from '../utils/valuationChartData';
